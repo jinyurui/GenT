d54c328395fa928443f3227acb98c863e45a40ba
Jin <yuruijin@amd.com>
2021-09-24 16:33:43 +0800

    test install custom

--- source/conf.py
+++ source/conf.py
@@ -14,8 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_copybutton
-
 # -- Project information -----------------------------------------------------
 
 project = 'GenT'
