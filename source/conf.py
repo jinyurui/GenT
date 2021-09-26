4b0cbfcdf1382c04fd0f81cac6b990eb5245fa09
Jin <yuruijin@amd.com>
2021-09-26 11:10:29 +0800

    add req package

--- source/conf.py
+++ source/conf.py
@@ -29,8 +29,7 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
-#['sphinx_copybutton']
+extensions = ['sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
