69b3aaa5465db999663f9066aeaf8fe0694a19b1
jyr <2464107061@qq.com>
2021-09-27 11:17:37 +0800

    add doxygen

---
+++ code/Test.hpp
@@ -0,0 +1,25 @@
+
+#pragma once
+
+/*!
+  @class Test
+  @brief this the test calss
+ */
+class Test
+{
+public:
+    /*!
+      new  
+     */
+    Test(){}
+ 
+    /*!
+      destory
+     */
+    virtual ~Test(){}
+
+    /*!
+      add
+    */
+    virtual void add() = 0
+};
---
+++ run_doxygen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ./doxygen
+doxygen Doxyfile
